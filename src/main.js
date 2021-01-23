d1337e28efa4634084bfa6186692007eabe3488e
leehs <504794559@qq.com>
2021-01-24 01:12:57 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios";
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
-
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = '127.0.0.1:8000'
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
